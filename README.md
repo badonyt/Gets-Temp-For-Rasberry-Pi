747b5d064eb08b7a916165f3f5cc77478912f2c6
bad <soccerskin87@gmail.com>
2022-12-27 09:19:55 +0000

    Initial Commit Actually

---
+++ README.md
@@ -0,0 +1,6 @@
+# Gets Temp For Rasberry Pi
+Gets the tempertature of Rasberry Pi at all times
+
+# Pip packages(SERVER)
+<code>pip install flask</code><br>
+<code>pip install gevent</code><br>
---
+++ server/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from gevent.pywsgi import WSGIServer
+
+app = Flask(__name__)
+
+def gettemp():
+    cpu_temp = os.popen("vcgencmd measure_temp").readline()
+    return cpu_temp.replace("temp=", "")
+
+@app.route('/', methods=['GET'])
+def hello():
+    return str(gettemp())
+if __name__ == '__main__':
+    # Debug/Development
+    # app.run(debug=True, host="0.0.0.0", port="5000")
+
+    # Production
+    http_server = WSGIServer(('0.0.0.0', 3000), app)
+    http_server.serve_forever()
