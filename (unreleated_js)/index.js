0fc17d94a709da93e24aafd125ebd754e3ed0ff3
BAD <soccerskin87@gmail.com>
2023-01-01 22:42:28 +0000

    Added js example

---
+++ (unreleated_js)/index.js
@@ -0,0 +1,13 @@
+const { exec } = require("child_process");
+
+exec("usr/bin/vcgencmd measure_temp", (error, stdout, stderr) => {
+    if(error){
+        console.log("Error"+error)
+        return;
+    }
+    if(stderr){
+        console.log(stderr)
+        return;
+    }
+    console.log("stdout: " + stdout)
+})
